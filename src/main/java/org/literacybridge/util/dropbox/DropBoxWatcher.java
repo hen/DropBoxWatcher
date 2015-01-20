a1aeb2027a03e2ac98e6061173a5cb3f9ca9e671
Hen <hen@flamefew.com>
2015-01-19 18:49:45 -0800

    Initial proof of concept

---
+++ src/main/java/org/literacybridge/util/dropbox/DropBoxWatcher.java
@@ -0,0 +1,55 @@
+// Include the Dropbox SDK.
+import com.dropbox.core.*;
+import java.io.*;
+import java.util.Locale;
+
+public class DropBoxWatcher {
+
+    public static void main(String[] args) throws IOException, DbxException {
+        // Get your app key and secret from the Dropbox developers website.
+        final String APP_KEY = "NEEDS SETTING";
+        final String APP_SECRET = "NEEDS SETTING";
+
+        DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
+
+        DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0",
+            Locale.getDefault().toString());
+
+        // If token unknown, use the authFirstTime method
+        // String accessToken = authFirstTime(appInfo, config);
+        String accessToken = "NEEDS SETTING";
+        DbxClient client = new DbxClient(config, accessToken);
+
+        // NOTE: No encoding handling here. 
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            DbxEntry.File downloadedFile = client.getFile("/regions.txt", null,
+                outputStream);
+            System.out.println("Metadata: " + downloadedFile.toStringMultiline());
+            System.out.println("Data: " + outputStream);
+        } finally {
+            outputStream.close();
+        }
+        
+        // return outputStream.toString();
+
+    }
+
+    public static String authFirstTime(DbxAppInfo appInfo, DbxRequestConfig config) throws IOException, DbxException {
+        DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+
+        // Have the user sign in and authorize your app.
+        String authorizeUrl = webAuth.start();
+        System.out.println("1. Go to: " + authorizeUrl);
+        System.out.println("2. Click \"Allow\" (you might have to log in first)");
+        System.out.println("3. Copy the authorization code.");
+        String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+
+        // This will fail if the user enters an invalid authorization code.
+        DbxAuthFinish authFinish = webAuth.finish(code);
+        String accessToken = authFinish.accessToken;
+
+        System.out.println("Access Token: '" + accessToken + "'");
+        return accessToken;
+    }
+}
